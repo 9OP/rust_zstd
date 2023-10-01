8c6a87ad8bb5da0191dec6fd957cb61a23024927
Martin <martin-guyard@live.fr>
2023-10-02 00:04:36 +0200

    Add unit tests

--- src/zstd/parsing.rs
+++ src/zstd/parsing.rs
@@ -6,39 +6,115 @@ impl<'a> ForwardByteParser<'a> {
         Self(data)
     }
 
-    // returns parsed u8
+    /// Pop and return (side-effect)
     pub fn u8(&mut self) -> Result<u8> {
+        // let (first, rest) = match self.0.split_first() {
+        //     Some(v) => v,
+        //     None => {
+        //         return Result::Err(Error::NotEnoughBytes {
+        //             requested: 1,
+        //             available: 0,
+        //         })
+        //     }
+        // };
+        // equivalent
         let (first, rest) = self.0.split_first().ok_or(Error::NotEnoughBytes {
             requested: 1,
             available: 0,
         })?;
         self.0 = rest;
-        return Ok(*first);
+        Ok(*first)
     }
-
-    // pub fn u8(&mut self) -> Option<u8> {
-    //     let (first, rest) = self.0.split_first()?;
-    //     self.0 = rest;
-    //     Ok(*first)
-    // }
 
     /// Return the number of bytes still unparsed
     pub fn len(&self) -> usize {
-        todo!()
+        self.0.len()
     }
 
     /// Check if the input is exhausted
     pub fn is_empty(&self) -> bool {
-        todo!()
+        self.len() == 0
     }
 
     /// Extract `len` bytes as a slice
     pub fn slice(&mut self, len: usize) -> Result<&'a [u8]> {
-        todo!()
+        match len <= self.len() {
+            true => Ok(&self.0[0..len]),
+            false => Result::Err(Error::NotEnoughBytes {
+                requested: len,
+                available: self.len(),
+            }),
+        }
     }
 
     /// Consume and return a u32 in little-endian format
     pub fn le_u32(&mut self) -> Result<u32> {
-        todo!()
+        let u = self.u8()? as u32;
+        Ok(u.to_le())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_u8() {
+        let mut parser = ForwardByteParser::new(&[0x12, 0x23, 0x34]);
+        assert_eq!(0x12, parser.u8().unwrap());
+        assert_eq!(0x23, parser.u8().unwrap());
+        assert_eq!(0x34, parser.u8().unwrap());
+        assert!(matches!(
+            parser.u8(),
+            Err(Error::NotEnoughBytes {
+                requested: 1,
+                available: 0,
+            })
+        ));
+    }
+
+    #[test]
+    fn test_len() {
+        let parser = ForwardByteParser::new(&[0x12, 0x23, 0x34]);
+        assert_eq!(3, parser.len());
+        let parser = ForwardByteParser::new(&[]);
+        assert_eq!(0, parser.len());
+    }
+
+    #[test]
+    fn test_is_empty() {
+        let parser: ForwardByteParser<'_> = ForwardByteParser::new(&[0x12, 0x23, 0x34]);
+        assert_eq!(false, parser.is_empty());
+        let parser = ForwardByteParser::new(&[]);
+        assert_eq!(true, parser.is_empty());
+    }
+
+    #[test]
+    fn test_slice() {
+        let mut parser: ForwardByteParser<'_> = ForwardByteParser::new(&[0x12, 0x23, 0x34]);
+        assert_eq!(&[0x12, 0x23], parser.slice(2).unwrap());
+        assert_eq!(&[0x12, 0x23, 0x34], parser.slice(3).unwrap());
+        assert!(matches!(
+            parser.slice(4),
+            Err(Error::NotEnoughBytes {
+                requested: 4,
+                available: 3,
+            })
+        ));
+    }
+
+    #[test]
+    fn test_le_u32() {
+        let mut parser: ForwardByteParser<'_> = ForwardByteParser::new(&[0x12, 0x23, 0x34]);
+        assert_eq!(0x12 as u32, parser.le_u32().unwrap());
+        assert_eq!(0x23 as u32, parser.le_u32().unwrap());
+        assert_eq!(0x34 as u32, parser.le_u32().unwrap());
+        assert!(matches!(
+            parser.le_u32(),
+            Err(Error::NotEnoughBytes {
+                requested: 1,
+                available: 0,
+            })
+        ));
     }
 }
--- tests/parsing.rs
+++ tests/parsing.rs
@@ -1,17 +1,18 @@
-use net7212::zstd::{errors::Error, parsing::ForwardByteParser};
+// implements integrations tests here
+// use net7212::zstd::{errors::Error, parsing::ForwardByteParser};
 
-#[test]
-fn forward_byte_parser_u8() {
-    // Check that bytes are delivered in order
-    let mut parser = ForwardByteParser::new(&[0x12, 0x23, 0x34]);
-    assert_eq!(0x12, parser.u8().unwrap());
-    assert_eq!(0x23, parser.u8().unwrap());
-    assert_eq!(0x34, parser.u8().unwrap());
-    assert!(matches!(
-        parser.u8(),
-        Err(Error::NotEnoughBytes {
-            requested: 1,
-            available: 0,
-        })
-    ));
-}
+// #[test]
+// fn forward_byte_parser_u8() {
+//     // Check that bytes are delivered in order
+//     let mut parser = ForwardByteParser::new(&[0x12, 0x23, 0x34]);
+//     assert_eq!(0x12, parser.u8().unwrap());
+//     assert_eq!(0x23, parser.u8().unwrap());
+//     assert_eq!(0x34, parser.u8().unwrap());
+//     assert!(matches!(
+//         parser.u8(),
+//         Err(Error::NotEnoughBytes {
+//             requested: 1,
+//             available: 0,
+//         })
+//     ));
+// }
