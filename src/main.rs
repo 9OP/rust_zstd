7a3913477d11d710290586f748ef19e9ec1bd14c
Martin <martin-guyard@live.fr>
2023-12-08 11:27:42 +0100

    Update cli

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 use clap::Parser;
-use std::fs;
+use std::{fs, io::Write};
 
 #[derive(Parser, Debug)]
 #[command(version)]
 struct Args {
     /// Source file to decompress
     source: String,
-
-    /// Destination file
-    destination: String,
 
     /// Dump information about frames instead of outputing the result
     #[arg(short, long, default_value_t = false)]
@@ -22,11 +19,9 @@ fn main() -> eyre::Result<()> {
     let bytes = fs::read(args.source)?;
 
     let decoded = zstd_lib::decode(bytes, args.info)?;
-    fs::write(args.destination, decoded)?;
-    // {
-    // let mut stdout = std::io::stdout().lock();
-    // stdout.write_all(decoded.as_slice()).unwrap();
-    // }
+
+    let mut stdout = std::io::stdout().lock();
+    stdout.write_all(decoded.as_slice()).unwrap();
 
     Ok(())
 }
